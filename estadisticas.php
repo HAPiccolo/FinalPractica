3195f559e074b34d594774037282b793fe729d5b
hapiccolo <hapiccolo86@gmail.com>
2025-11-07 23:38:15 -0300

    Agregado stylesheet

--- estadisticas.php
+++ estadisticas.php
@@ -82,6 +82,8 @@ $yearly = $pdo->query($sqlYearly)->fetchAll(PDO::FETCH_ASSOC);
     <title>Estadísticas - Traumatología</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
